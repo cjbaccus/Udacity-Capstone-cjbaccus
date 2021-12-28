4e431ce03991dd62fb07f5fda12ff4b9646045bc
cjbaccus <cjbaccus@gmail.com>
2021-12-28 04:45:17 -0700

    show new numbers

--- scripts/cryptotest.py
+++ scripts/cryptotest.py
@@ -6,7 +6,7 @@ from cryptocmd import CmcScraper
 #import pandas
 
 # initialise scraper without time interval
-scraper = CmcScraper("ETH", "23-09-2021", "20-10-2021")
+scraper = CmcScraper("HNT", "23-09-2021", "27-12-2021")
 
 # get raw data as list of list
 headers, data = scraper.get_data()
@@ -15,7 +15,7 @@ headers, data = scraper.get_data()
 eth_json_data = scraper.get_data("json")
 
 # export the data as csv file, you can also pass optional `name` parameter
-scraper.export("csv", name="eth_all_time")
+scraper.export("csv", name="HNT_all_time")
 
 # Pandas dataFrame for the same data
 df = scraper.get_dataframe()
