c5c8ae0cb88b781f7121a67578568d6b691d2e8c
cjbaccus <cjbaccus@gmail.com>
2022-01-01 14:15:28 -0700

    delete green app, make extras, add crypto

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -5,18 +5,11 @@
 # docker login
 # Step 1:
 # Build image and add a descriptive tag
-docker build . -t cjbaccus/${DOCKER_PATH}:${CURRENT} -f Dockerfile
+docker build . -t ${DOCKER_PATH}:${CURRENT} -f Dockerfile
 
 # docker build . --tag ${DOCKER_PATH}:${CURRENT}
 
 # Step 2: 
 # List docker images
 # docker login
-# docker image ls
-
-# Step 3: 
-# Run flask app
-# docker run -p 80:80 mycryptoflask
-# docker run -p 80:80 $DOCKER_USER/cjbaccus-capstone:latest
-# docker image push cjbaccus/cjbaccus-capstone:latest
-# docker push ${DOCKER_PATH}:${LAST}
+docker image ls
