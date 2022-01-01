2c1428c2d38314f4afeb007e659f24509a4dc965
cjbaccus <cjbaccus@gmail.com>
2022-01-01 12:15:32 -0700

    just build

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -5,7 +5,7 @@
 # docker login
 # Step 1:
 # Build image and add a descriptive tag
-docker build . --tag=cjbaccus/cjbaccus-capstone:1
+docker build .
 
 # docker build . --tag ${DOCKER_PATH}:${CURRENT}
 
