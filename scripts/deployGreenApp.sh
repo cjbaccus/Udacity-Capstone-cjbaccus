f2507f44a9f0b6a6a5defafb472bb856a58e6ed9
cjbaccus <cjbaccus@gmail.com>
2022-01-01 15:42:31 -0700

    try this

--- scripts/deployGreenApp.sh
+++ scripts/deployGreenApp.sh
@@ -2,4 +2,5 @@
 
 . ./config.txt
 kubectl create deployment crypto-${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --port=8080
+kubectl expose deployment crypto-${CURRENT} --port=8080 --target-port=8080 --save-config
 kubectl rollout status deployment/crypto-${CURRENT} # Health check
