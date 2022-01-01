09668cd1fde6955773149796d3ae95055012e26d
cjbaccus <cjbaccus@gmail.com>
2022-01-01 15:38:53 -0700

    fix green app

--- scripts/deployGreenApp.sh
+++ scripts/deployGreenApp.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 . ./config.txt
-kubectl create deployment crypto-${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --port=80
+kubectl create deployment crypto-${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --port=8080
 kubectl rollout status deployment/crypto-${CURRENT} # Health check
