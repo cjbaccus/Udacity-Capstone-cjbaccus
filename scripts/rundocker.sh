3de635cfe38b3a500b2d172bfab825aa92415d3f
cjbaccus <cjbaccus@gmail.com>
2022-01-01 09:26:47 -0700

    try again

--- scripts/rundocker.sh
+++ scripts/rundocker.sh
@@ -4,7 +4,7 @@
 docker login -u $DOCKER_USER -p $DOCKER_PASSWORD
 # Step 1:
 # Build image and add a descriptive tag
-docker build --tag=cjbaccus-capstone .
+docker build --tag=cjbaccus/cjbaccus-capstone:latest .
 
 # Step 2: 
 # List docker images
@@ -14,4 +14,5 @@ docker image ls
 # Step 3: 
 # Run flask app
 # docker run -p 80:80 mycryptoflask
-docker run -p 80:80 $DOCKER_USER/cjbaccus-capstone
+docker run -p 80:80 $DOCKER_USER/cjbaccus-capstone:latest
+docker push cjbaccus/cjbaccus-capstone:latest
