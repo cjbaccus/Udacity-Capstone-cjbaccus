819520b6e3509334b5bac4055b4dff0a9265c2a4
Carl Baccus <cjbaccus@gmail.com>
2022-01-01 21:19:24 -0700

    try with cloudformation

--- scripts/kubernetesDeploy.sh
+++ scripts/kubernetesDeploy.sh
@@ -30,3 +30,9 @@ kubectl port-forward cjbaccus/capstone 8080:8080
 
 # POD_NAME=`kubectl get pods -o go-template --template '{{range .items}}{{.metadata.name}}{{"\n"}}{{end}}'`
 # kubectl port-forward $POD_NAME 8080:8080
+#!/bin/sh
+
+. ./config.txt
+kubectl create deployment crypto-${CURRENT} --image=docker.io/${DOCKER_PATH}:${CURRENT} --replicas=2 --port=80
+kubectl expose deployment crypto-${CURRENT} --type=LoadBalancer --port=80 --target-port=8080 --save-config
+kubectl rollout status deployment/crypto-${CURRENT} # Health check
