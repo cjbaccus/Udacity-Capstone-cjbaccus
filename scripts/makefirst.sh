b8a877a0bc47a24b2456fb5d85d337bfd784d139
Carl Baccus <cjbaccus@gmail.com>
2022-01-02 00:10:09 -0700

    make

---
+++ scripts/makefirst.sh
@@ -0,0 +1,22 @@
+
+#!/bin/sh
+
+. ./config.txt
+# deploy app into k8s
+kubectl create deployment crypto-${LAST} --image=docker.io/${DOCKER_PATH}:${LAST} --replicas=2 --port=80
+kubectl rollout status deployment/crypto-${LAST} # Health check
+
+# setup blue load balancer
+kubectl expose deployment crypto-${LAST} --type=LoadBalancer --port=80 --target-port=8080 --name=blue-lb --save-config
+
+while [ 1 ]
+do
+   URL=$(kubectl get services blue-lb -o=jsonpath='{.status.loadBalancer.ingress[0].hostname}')
+   if [ ! -z $URL ]
+   then
+      break
+   fi
+   echo "waiting for url...\n"
+   sleep 1
+done
+echo $URL
