c3132ecedf92646ea29140a5b713d38e976cc207
Carl Baccus <cjbaccus@gmail.com>
2022-01-03 10:26:09 -0700

    fix makefirst

--- scripts/makefirst.sh
+++ scripts/makefirst.sh
@@ -3,17 +3,17 @@
 
 . ./config.txt
 
-kubectl expose -h
-kubeadm init
+# kubectl expose -h
+# kubeadm init
 # deploy app into k8s
 # kubectl config set-cluster crypto-${LAST} --server=http://localhost:8080
 # kubectl config set-context crypto-system-${LAST} --cluster=crypto-${LAST}
 # kubectl config use-context crypto-system-${LAST}
-# kubectl create deployment crypto-${LAST} --image=docker.io/${DOCKER_PATH}:${LAST} --replicas=2 --port=80
-# kubectl rollout status deployment/crypto-${LAST} # Health check
+kubectl create deployment crypto-${LAST} --image=docker.io/${DOCKER_PATH}:${LAST} --replicas=2 --port=80
+kubectl rollout status deployment/crypto-${LAST} # Health check
 
 # # setup blue load balancer
-# kubectl expose deployment crypto-${LAST} --type=LoadBalancer --port=80 --target-port=8080 --name=blue-lb --save-config
+kubectl expose deployment crypto-${LAST} --type=LoadBalancer --port=80 --target-port=8080 --name=blue-lb --save-config
 
 # while [ 1 ]
 # do
