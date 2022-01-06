8fb7ce0a9e24e2947c3a03fed304accf4ff8b657
Carl Baccus <cjbaccus@gmail.com>
2022-01-06 15:04:54 -0700

    test env variables for eks

--- scripts/buildeks.sh
+++ scripts/buildeks.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+echo "[default]\ 
+aws_access_key_id=$AWS_ACCESS_KEY_ID\
+aws_secret_access_key=$AWS_SECRET_ACCESS_KEY" > ~/.aws/credentials
+
 eksctl create cluster \
 --name crypto-2 \
 --region us-east-1 \
